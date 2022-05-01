ba1c4b25db1146ec5997c63b2157ec60645881d5
Ewan Hickling <hicklingewan@icloud.com>
2022-05-01 13:52:13 +0200

    Added comments

--- MATLAB/main_system.m
+++ MATLAB/main_system.m
@@ -7,33 +7,24 @@ clf
 t = linspace(0,1000,1000);
 
 %% Define Parameters 
-% Will be leaving this for now. The parameters don't affect the solvability
-% of the system.
-
-p.N = 4;      % ~, number of trays. #TML:note that I provide units (in this case none, ~) and a description
+p.N = 4;      % ~, Number of trays. 
 
 %% Define exogenous variables
-%  Feed variables
-u.LF = @(t) 10 + 1*(t > 300);
-u.XF = @(t) 0.5 + 0*t;
+% Feed variables
+u.LF = @(t) 10 + 1*(t > 300);       % mol/min, Feed liquid molar flowrate
+u.XF = @(t) 0.5 + 0*t;              % ~, Feed liquid molar fraction 
 
-%  Desired reflux 
-u.LR = @(t) 8.53 + 2*(t > 100);
+% Desired reflux 
+u.LR = @(t) 8.53 + 2*(t > 100);     % mol/min, Reflux molar flowrate
 
-%  Molar Holdup
-u.MMB = @(t) 10 + 0*t;
-u.MMD = @(t) 10 + 0*t;
-
-%  Heat transfer rates across trays
+% Heat transfer rates across trays
 for n = 1 : p.N
-    u.Q{n} = @(t) 0 + 0*t;      % #TML: Cells { } are a useful way of storing things other than numbers, like functions
+    u.Q{n} = @(t) 0 + 0*t;          % kJ/mol, Heat transfer rate for each tray
 end
-u.Freb = @(t) 2 + 0*t;
+u.Freb = @(t) 2 + 0*t;              % mol/min, Boiler heating fluid molar flowrate
 
 %% Define intial conidtions - Molar Holdup
 % Initial conditions of the molar holdup ODEs
-% #TML: I suggest simplifying things, don't use structures for your state
-% variables
 MM0 = ones(p.N, 1);
 
 %% Simulate ODEs - Molar Holdup
@@ -64,7 +55,3 @@ legend(labels, 'location', 'best')
 % %% Simulate ODEs - Concentrations
 % % This solves the liquid mole fraction ODEs
 % sol1 = ode45(@(t, y) simulate_ODEs1(t, x, vM, u, p), [0 1000], x0X_vec)
-
-
-
-
