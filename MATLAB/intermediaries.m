d748f59d8508af7789f6bc8d9734fd0eb952d18a
Ewan Hickling <hicklingewan@icloud.com>
2022-05-10 06:07:57 +0200

    Added vapour flowrate calculations

--- MATLAB/intermediaries.m
+++ MATLAB/intermediaries.m
@@ -4,22 +4,24 @@ function v = intermediaries(t, x, u, p)
 % Define the dependent variable
     MM = x(1:p.N+2);                % mol/min, Molar holdup
 
-% Calculate Heat Transfer duties
+% Define Reboiler duty
     v.Qreb = p.kr*u.Freb(t);        % kJ/mol, Reboiler heat transfer duty
-    v.Qcon = p.yy*v.V(4);           % kJ/mol, Condenser heat transfer duty
-
+  
 % Calculate Vapour molar flowrates
     v.V(1) = v.Qreb/p.yy;                       % mol/min, Vapour molar flowrate - V0
     for n = 2:p.N
         v.V(n) = v.V(n-1) + u.Q{n}(t)/p.yy;     % mol/min, Vapour molar flowrate - V1:V4
-    end
+    end 
 
 % Calculate Liquid molar flowrates
     for n = 1:p.N
         v.L(n) = sqrt(MM(n));       % mol/min, Liquid molar flowrate - L1:L4
     end
-    v.LB = v.L(1) - v.V0;           % mol/min, Liquid molar flowrate - LB 
-    v.LR = v.V4./(1 + u.R(t));      % mol/min, Liquid molar flowrate - LR
+    v.LB = v.L(1) - v.V(1);         % mol/min, Liquid molar flowrate - LB 
+    v.LR = v.V(4)./(1 + u.R(t));    % mol/min, Liquid molar flowrate - LR
     v.LD = v.LR/u.R(t);             % mol/min, Liquid molar flowrate - LD
-    
+
+% Calculate Condenser duty
+    v.Qcon = p.yy*v.V(4);           % kJ/mol, Condenser heat transfer duty
+
 end 
