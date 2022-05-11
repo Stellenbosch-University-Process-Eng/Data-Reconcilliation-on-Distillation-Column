e9406cd13a4f05395babf67d23c91a37a9723b8f
tmlouw <tmlouw@sun.ac.za>
2022-05-11 09:18:58 +0200

    Corrected vector notation for calculation of intermediate variables.

--- MATLAB/intermediaries.m
+++ MATLAB/intermediaries.m
@@ -5,20 +5,20 @@ function v = intermediaries(t, x, u, p)
     MM = x(1:p.N+2);                % mol/min, Molar holdup
   
 % Calculate Vapour molar flowrates
-    v.V(1) = u.Freb(t)/p.yy;                    % mol/min, Vapour molar flowrate - V0
+    v.V(1,:) = u.Freb(t)/p.yy;                    % mol/min, Vapour molar flowrate - V0
     for n = 2:p.N
-        v.V(n) = v.V(n-1) + u.Q{n}(t)/p.yy;     % mol/min, Vapour molar flowrate - V1:V4
+        v.V(n,:) = v.V(n-1) + u.Q{n}(t)/p.yy;     % mol/min, Vapour molar flowrate - V1:V4
     end 
 
 % Calculate Liquid molar flowrates
     for n = 1:p.N
-        v.L(n) = sqrt(MM(n));       % mol/min, Liquid molar flowrate - L1:L4
+        v.L(n,:) = sqrt(MM(n));       % mol/min, Liquid molar flowrate - L1:L4
     end
-    v.LB = v.L(1) - v.V(1);         % mol/min, Liquid molar flowrate - LB 
-    v.LR = v.V(4)./(1 + u.R(t));    % mol/min, Liquid molar flowrate - LR
-    v.LD = v.LR/u.R(t);             % mol/min, Liquid molar flowrate - LD
+    v.LB = v.L(1,:) - v.V(1,:);         % mol/min, Liquid molar flowrate - LB 
+    v.LR = v.V(p.N,:)./(1 + u.R(t));    % mol/min, Liquid molar flowrate - LR
+    v.LD = v.LR./u.R(t);             % mol/min, Liquid molar flowrate - LD
 
 % Calculate Condenser duty
-    v.Qcon = p.yy*v.V(4);           % kJ/mol, Condenser heat transfer duty
+    v.Qcon = p.yy*v.V(p.N,:);           % kJ/mol, Condenser heat transfer duty
 
 end 
