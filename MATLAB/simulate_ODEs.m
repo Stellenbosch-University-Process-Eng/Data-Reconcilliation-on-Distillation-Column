f68b061bbf074829398c81c950a77d25ed83e758
Ewan Hickling <hicklingewan@icloud.com>
2022-05-18 12:06:08 +0200

    Divided mole fraction ODEs with molar holdup. This slows down execution immensely.

--- MATLAB/simulate_ODEs.m
+++ MATLAB/simulate_ODEs.m
@@ -17,13 +17,13 @@ function dxdt = simulate_ODEs(t, x, u, p)
 
 % Calculate Liquid fracion derivatives
     ddt_X = zeros(p.N, 1);
-    ddt_X(1) = v.L(2).*(MM(2+p.N)) + v.L(1).*(1-MM(1+p.N)) - v.V(1).*v.Y(1) + v.V0.*v.Y0 + u.Q{n}(t).*MM(1+p.N);
+    ddt_X(1) = (v.L(2).*(MM(2+p.N)) + v.L(1).*(1-MM(1+p.N)) - v.V(1).*v.Y(1) + v.V0.*v.Y0 + u.Q{n}(t).*MM(1+p.N))./MM(1);
     for n = 2:p.N-1
-        ddt_X(n) = v.L(n+1)*(MM(n+1+p.N)) + v.L(n)*(1-MM(n+p.N)) - v.V(n)*v.Y(n) + v.V(n-1)*v.Y(n-1) + u.Q{n}(t).*MM(n+p.N);
+        ddt_X(n) = (v.L(n+1)*(MM(n+1+p.N)) + v.L(n)*(1-MM(n+p.N)) - v.V(n)*v.Y(n) + v.V(n-1)*v.Y(n-1) + u.Q{n}(t).*MM(n+p.N))./MM(n);
     end
-    ddt_X(p.N) = v.LR*(MM(p.N*2+2)) + v.L(p.N)*(1-MM(p.N*2)) - v.V(p.N)*v.Y(p.N) + v.V(p.N-1)*v.Y(p.N-1) + u.Q{p.N}(t).*MM(p.N*2);
-    ddt_XB = v.L(1)*MM(1+p.N) - v.LB*MM(p.N*2+1) - v.V0*v.Y0;
-    ddt_XD = v.V(p.N) * (v.Y(p.N) - MM(p.N*2+2));
+    ddt_X(p.N) = (v.LR*(MM(p.N*2+2)) + v.L(p.N)*(1-MM(p.N*2)) - v.V(p.N)*v.Y(p.N) + v.V(p.N-1)*v.Y(p.N-1) + u.Q{p.N}(t).*MM(p.N*2))./MM(p.N);
+    ddt_XB = (v.L(1)*MM(1+p.N) - v.LB*MM(p.N*2+1) - v.V0*v.Y0)/10;
+    ddt_XD = (v.V(p.N) * (v.Y(p.N) - MM(p.N*2+2)))/10;
     dxdt = [ddt_MM; ddt_X; ddt_XB; ddt_XD];
 
 end
