a4976676fb7522471c3ae3059bbe473a03f0c9e4
Ewan Hickling <hicklingewan@icloud.com>
2022-08-09 13:06:14 +0200

    Added function that generates the variance matrix

---
+++ MATLAB/varianceMatrix.m
@@ -0,0 +1,13 @@
+function W = varianceMatrix(a, var)
+% This function generates the variance matrix W
+% It has two inputs, a & v
+% a = the amount of measured variables
+% v = variance associated with each measured variable
+W = eye(a);
+for i = 1:a
+    for j = 1:a
+        if W(i,j) ~= 0
+            W(i,j) = var;
+        end    
+    end
+end
