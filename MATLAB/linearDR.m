55a9c28a99cfbd866a9743b074e217e289e89ea3
Ewan Hickling <hicklingewan@icloud.com>
2022-08-25 06:28:50 +0200

    Updated display of results

--- MATLAB/linearDR.m
+++ MATLAB/linearDR.m
@@ -57,7 +57,7 @@ LB_avm = xhat(5,:);
 % based on the number of unmeasured variables
 LB_svm = zeros(4,1001);
 for i = 1:4
-   [LB_svm(i,:), Obs(i,:)] = generateLB_svm(i, measured_data, variance);
+   LB_svm(i,:) = generateLB_svm(i, measured_data, variance);
 end
 
 %% Error metrics
@@ -77,50 +77,94 @@ for i = 1:4
     res_svm(i,:) = true_data.LB - LB_svm(i,:);
 end
 
-%% Display results
-% Plot results - Line Graph
-% subplot(5,1,1)
-% plot(time(100:end,:), true_data.LB(:,100:end), 'co', time(100:end,:), measured_data.LB(:,100:end), 'y', time(100:end,:), LB_avm(:,100:end), 'k')
-% title("All Variables Measured")
-% xlabel('Time (s)'); ylabel('LB');
-% legend('Model', "Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_avm)+"%", 'Location', 'Best')
-% 
-% for i = 1:4
-%     subplot(5,1,i+1)
-%     plot(time(100:end,:), true_data.LB(:,100:end), 'co', time(100:end,:), measured_data.LB(:,100:end), 'y', time(100:end,:), LB_svm(i,100:end), 'k')
-%     title("Some Variables Measured: No. Unmeasured = "+num2str(i))
-%     xlabel('Time (s)'); ylabel('LB');
-%     legend('Model', "Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_svm(i,1))+"%", 'Location', 'Best')
-% end
-
-% Plot results - Histogram
-subplot(5,1,1)
-histogram(resM(:,100:end),'FaceAlpha',0.1)
-hold on
-histogram(res_avm(:,100:end),'FaceAlpha',1)
-hold off
-title("All Variables Measured")
-xlabel('Time (s)'); ylabel('LB');
-legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_avm)+"%", 'Location', 'Best')
+% Density functions
+[fm, xim] = ksdensity(resM(:,100:end))
+[favm, xiavm] = ksdensity(res_avm(:,100:end))
+fsvm = zeros(1,100); xisvm = zeros(1,100);
 for i = 1:4
-    subplot(5,1,i+1)
-    histogram(resM(:,100:end),'FaceAlpha',0.1)
-    hold on
-    histogram(res_svm(i,100:end),'FaceAlpha',1)
-    hold off
-    if i == 1
-        title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1")
-    elseif i == 2
-        title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1 & V4")
-    elseif i == 3
-        title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4 & L2")
-    elseif i == 4
-        title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4, L2 & V3")
-    end        
-    xlabel('Time (s)'); ylabel('LB');
-    legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_svm(i))+"%", 'Location', 'Best')
+    [fsvm(i,:), xisvm(i,:)] = ksdensity(res_svm(i,100:end));
 end
-sgtitle("Histograms of the residuals for the measurements & the reconciled values")
+
+%% Display results
+% Choose type of display
+% Line graph               >> disp = 1
+% Histogram                >> disp = 2
+% Probability distribution >> disp = 3
+disp = 3;
+
+% Plot results - Line Graph
+if disp == 1
+    subplot(5,1,1)
+    plot(time(100:end,:), true_data.LB(:,100:end), 'co', time(100:end,:), measured_data.LB(:,100:end), 'y', time(100:end,:), LB_avm(:,100:end), 'k')
+    title("All Variables Measured")
+    xlabel('Time (s)'); ylabel('LB');
+    legend('Model', "Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_avm)+"%", 'Location', 'bestoutside')
+
+    for i = 1:4
+        subplot(5,1,i+1)
+        plot(time(100:end,:), true_data.LB(:,100:end), 'co', time(100:end,:), measured_data.LB(:,100:end), 'y', time(100:end,:), LB_svm(i,100:end), 'k')
+        title("Some Variables Measured: No. Unmeasured = "+num2str(i))
+        xlabel('Time (s)'); ylabel('LB');
+        legend('Model', "Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_svm(i,1))+"%", 'Location', 'bestoutside')
+    end
+    sgtitle("Measurements vs the reconciled values")
+    
+% Plot results - Histogram
+elseif disp == 2
+    subplot(5,1,1)
+    histogram(resM(:,100:end),50,'FaceAlpha',0.1)
+    hold on
+    histogram(res_avm(:,100:end),50,'FaceAlpha',1)
+    hold off
+    title("All Variables Measured")
+    xlabel('Time (s)'); ylabel('LB');
+    legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_avm)+"%", 'Location', 'Best')
+    for i = 1:4
+        subplot(5,1,i+1)
+        histogram(resM(:,100:end),50,'FaceAlpha',0.1)
+        hold on
+        histogram(res_svm(i,100:end),50,'FaceAlpha',1)
+        hold off
+        if i == 1
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1")
+        elseif i == 2
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1 & V4")
+        elseif i == 3
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4 & L2")
+        elseif i == 4
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4, L2 & V3")
+        end        
+        xlabel('Time (s)'); ylabel('LB');
+        legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_svm(i))+"%", 'Location', 'Best')
+    end
+    sgtitle("Histograms of the residuals for the measurements & the reconciled values")
+end
+
+% Plot results - Density Function
+if disp == 3
+    subplot(5,1,1)
+    plot(xim, fm, 'y', xiavm, favm, 'b')
+    title("All Variables Measured")
+    xlabel('Time (s)'); ylabel('LB');
+    legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_avm)+"%", 'Location', 'Best')
+    for i = 1:4
+        subplot(5,1,i+1)
+        plot(xim, fm, 'y', xisvm(i,:), fsvm(i,:), 'b')
+        if i == 1
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1")
+        elseif i == 2
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> L1 & V4")
+        elseif i == 3
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4 & L2")
+        elseif i == 4
+            title("Some Variables Measured: No. Unmeasured = "+num2str(i)+" >> LF, V4, L2 & V3")
+        end        
+        xlabel('Time (s)'); ylabel('LB');
+        legend("Measurement with MAPE = "+num2str(mapeM)+"%", "Data Reconciliation with MAPE = "+num2str(mape_svm(i))+"%", 'Location', 'Best')
+    end
+    sgtitle("Probability distributions of the residuals for the measurements & the reconciled values")
+end
+
 % Display in MAPE in Table
 NumberUnmeasuredVariables = ["M";"0";"1";"2";"3";"4"];
 MAPE = [mapeM; mape_avm; mape_svm];
