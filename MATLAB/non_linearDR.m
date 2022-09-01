4f79473dc5b6891dd902da25ef21770205fdea72
Ewan Hickling <hicklingewan@icloud.com>
2022-09-01 11:22:08 +0200

    Updated non-linear to include both plots

--- MATLAB/non_linearDR.m
+++ MATLAB/non_linearDR.m
@@ -16,7 +16,7 @@ Xhat = zeros(25,1001);
 XB   = zeros(4,1001);
 
 %% Observability analysis
-for i = 1:3
+for i = 1:5
     % Set Up Matrices
     v = i-1; X0 = zeros(25,1);
     [Y, index] = measurements_Y(v, measured_data);
@@ -52,30 +52,54 @@ res_avm = true_data.XB(:,100:end) - XB(1,100:end);
 [favm, xiavm] = ksdensity(res_avm);
 
 % Preallocate
-mape_svm = zeros(3,1);
-res_svm = zeros(3,902);
+mape_svm = zeros(4,1);
+res_svm = zeros(4,902);
 fsvm = zeros(1,100); xisvm = zeros(1,100);
-for i = 1:3
+for i = 1:4
     mape_svm(i) = mean(100*abs((true_data.XB(:,100:end) - XB(i+1,100:end))./true_data.XB(:,100:end)));
     res_svm (i,:) = true_data.XB(:,100:end) - XB(i+1,100:end);
     [fsvm(i,:), xisvm(i,:)] = ksdensity(res_svm(i,:));
 end
 
-%% Plot results
-subplot(4,1,1)
-plot(xim, fm, 'y', xiavm, favm, 'k')
-title("Probability distribution - All Variables Measured")
-xlabel("Residual Value"); ylabel("Probability")
-legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_avm)+"%")
+%% Display results
+% Choose type of display
+% Histogram                >> disp = 1
+% Probability distribution >> disp = 2
+disp = 2;
 
-for i = 1:3
-    subplot(4,1,i+1)
-    plot(xim, fm, 'y', xisvm(i,:), fsvm(i,:), 'k')
-    title("Probability distribution - No. = "+num2str(i))
+if disp == 1
+    subplot(5,1,1)
+    plot(xim, fm, 'y', xiavm, favm, 'k')
+    title("Probability distribution - All Variables Measured")
     xlabel("Residual Value"); ylabel("Probability")
-    legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_svm(i))+"%")
+    legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_avm)+"%")
+    for i = 1:4
+        subplot(5,1,i+1)
+        plot(xim, fm, 'y', xisvm(i,:), fsvm(i,:), 'k')
+        title("Probability distribution - No. = "+num2str(i))
+        xlabel("Residual Value"); ylabel("Probability")
+        legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_svm(i))+"%")
+    end 
+else
+    subplot(5,1,1)
+    histogram(resM,50,'FaceAlpha',0.1)
+    hold on
+    histogram(res_avm,50,'FaceAlpha',1)
+    hold off
+    title("Histogram - All Variables Measured")
+    xlabel("Residual Value"); ylabel("Frequency")
+    legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_avm)+"%")
+    for i = 1:4
+    subplot(5,1,i+1)
+        histogram(resM,50,'FaceAlpha',0.1)
+        hold on
+        histogram(res_svm(i,:),50,'FaceAlpha',1)
+        hold off
+        title("Histogram - No Unmeasured = "+num2str(i))
+        xlabel("Residual Value"); ylabel("Frequency")
+        legend("Measurement with MAPE = "+num2str(mapeM)+"%","Non-linear DR with MAPE = "+num2str(mape_svm(i))+"%")
+    end
 end
-
 
 %% Function
 function [g, f] = nonLinearConstraints(x)
@@ -102,7 +126,7 @@ function [Y, index] = measurements_Y(v, measured_data)
              measured_data.V2; measured_data.V3; measured_data.V4; measured_data.X1; measured_data.X2;...
              measured_data.X3; measured_data.X4; measured_data.XB; measured_data.XD; measured_data.XF;...
              measured_data.Y0; measured_data.Y1; measured_data.Y2; measured_data.Y3; measured_data.Y4];
-        index = [1:25]; 
+        index = (1:25); 
          
     elseif v == 1 % Remove >> Y4
         Y = [measured_data.L1; measured_data.L2; measured_data.L3; measured_data.L4; measured_data.LB;...
@@ -110,7 +134,7 @@ function [Y, index] = measurements_Y(v, measured_data)
              measured_data.V2; measured_data.V3; measured_data.V4; measured_data.X2; measured_data.X1;...
              measured_data.X3; measured_data.X4; measured_data.XB; measured_data.XD; measured_data.XF;...
              measured_data.Y0; measured_data.Y1; measured_data.Y2; measured_data.Y3];  
-        index = [1:24]; 
+        index = (1:24); 
          
     elseif v == 2 % Remove >> L4, X1, X4
         Y = [measured_data.L1; measured_data.L2; measured_data.L3; measured_data.LB;...
@@ -120,21 +144,21 @@ function [Y, index] = measurements_Y(v, measured_data)
              measured_data.Y0; measured_data.Y1; measured_data.Y2; measured_data.Y3; measured_data.Y4];
         index = [1:3,5:13,15:16,18:25]; 
          
-    elseif v == 3 % Remove >> L1, V3, X1, X4, Y0, Y4
-        Y = [measured_data.L2; measured_data.L3; measured_data.L4; measured_data.LB;...
+    elseif v == 3 % Remove >> L4, V3, X1, X4
+        Y = [measured_data.L1; measured_data.L2; measured_data.L3; measured_data.LB;...
              measured_data.LD; measured_data.LR; measured_data.LF; measured_data.V0; measured_data.V1;...
              measured_data.V2; measured_data.V4; measured_data.X2;...
              measured_data.X3; measured_data.XB; measured_data.XD; measured_data.XF;...
-             measured_data.Y1; measured_data.Y2; measured_data.Y3];
-        index = [2:11,13,15:16,18:20,22:24]; 
+             measured_data.Y0; measured_data.Y1; measured_data.Y2; measured_data.Y3; measured_data.Y4];
+        index = [1:3,5:11,13,15:16,18:25]; 
          
-    elseif v == 4 % Remove >> L1, V3, X1, X3, X4, Y0, Y4
-        Y = [measured_data.L2; measured_data.L3; measured_data.L4; measured_data.LB;...
+    elseif v == 4 % Remove >> L4, V3, X1, X4, Y2
+        Y = [measured_data.L1; measured_data.L2; measured_data.L3; measured_data.LB;...
              measured_data.LD; measured_data.LR; measured_data.LF; measured_data.V0; measured_data.V1;...
              measured_data.V2; measured_data.V4; measured_data.X2;...
-             measured_data.XB; measured_data.XD; measured_data.XF;...
-             measured_data.Y1; measured_data.Y2; measured_data.Y3];
-        index = [2:11,13,15,18:20,22:24]; 
+             measured_data.X3; measured_data.XB; measured_data.XD; measured_data.XF;...
+             measured_data.Y0; measured_data.Y1; measured_data.Y3; measured_data.Y4];
+        index = [1:3,5:11,13,15:16,18:22,24:25]; 
         
     end
 end
